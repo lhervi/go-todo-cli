cab70b7cff06b329d268b65f3e5c49ee017a0515
Lonirs Hervilla <lhervi@gmail.com>
2025-09-18 14:56:29 -0400

    feat: Add logic for JSON file persistence

---
+++ storage.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const tasksFile = "tasks.json"
+
+// SaveTasks guarda una lista de tareas en un archivo JSON.
+func SaveTask(tasks []Task) error {
+	data, err := json.MarshalIndent(tasks, "", " ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(tasksFile, data, 0644)
+}
+
+func LoadTasks() ([]Task, error) {
+	data, err := os.ReadFile(tasksFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []Task{}, nil
+		}
+		return nil, err
+	}
+	var tasks []Task
+	if err := json.Unmarshal(data, &tasks); err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
